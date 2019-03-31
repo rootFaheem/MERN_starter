41c93b5e16b3bb9d340709c535d0a5d49dad6d4c
Faheem <faheemtech1@gmail.com>
2019-03-31 21:18:43 +0530

    adding nodemon for constantly watching the server

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 
 const port = 5000;
 
+// @homepage route
 app.get("/", (req, res) => res.send("HOME PAGE"));
+app.get("/xyz", (req, res) => res.send("xyz abc"));
 
+// Server listen to PORT 5000
 app.listen(port, (req, res) => console.log(`server is running at ${port}...`));
