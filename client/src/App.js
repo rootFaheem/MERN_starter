0f64d5cdf9d471595314127ba1d84ccdb2ca864f
Faheem <faheemtech1@gmail.com>
2019-04-07 21:07:40 +0530

    some fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
+  login = () => {
+    const data = {
+      name: "abc",
+      age: 32
+    };
+
+    axios
+      .post("/user/", data)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => console.log(err));
+  };
+
   render() {
     return (
       <div className="App">
         <h1>React app</h1>
+        <div>
+          <button onClick={this.login}>click me</button>
+        </div>
       </div>
     );
   }
